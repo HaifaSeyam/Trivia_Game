ec54e2e44860d3a407a2c783c6651c445e410873
HaifaSeyam <h.abuamro@hotmail.com>
2018-12-23 16:12:24 -0600

    Trivia_Game Ready to be Played

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,67 @@ window.onload = function () {
 var timer = 31;
 var intervalId;
 
+/*Once Submit button is clicked, onSubmit function will be called
+In this function .. */
+function onSubmit(){
+    //.. Varibales declared for counting the correct and wrong answeres and unanswered questions
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
+    var unAnswered = 0;
+
+    //Number of questions as a counter to be used in the for loops
+    var numOfQuestions = 5;
+
+    //Array of answers
+    var answers = ["b", "b", "a", "c", "a"];
+
+    /*Variables to store the value of the selected input radio. Each question has 3 values, 
+    one for each input radio*/
+    var question1 = document.forms["triviaGame"]["question1"].value;
+    var question2 = document.forms["triviaGame"]["question2"].value;
+    var question3 = document.forms["triviaGame"]["question3"].value;
+    var question4 = document.forms["triviaGame"]["question4"].value;
+    var question5 = document.forms["triviaGame"]["question5"].value;
+
+    /*for loop to check if any of the radio buttons is not selected*/
+    for (var i = 1 ; i <= numOfQuestions ; i++){
+        if (eval("question" + i) == "") {
+            unAnswered++;
+            /*Each time I click on done button while not selecting any of the radio buttons, for testing purposes,
+            I got a result of 5 unanswered questions and 5 wrong answers at the same time .. 
+            when I tried to edit the if statements in the next for loop trying to fix this,
+            something wrong happens .. so here, each time I check for unselection
+            I increment the unAnswered counter and decrement the wrongAnswers counter, so whenever
+            I click on done button without selecting any radio button, I got a result of 
+            - No Answer: 5
+            - Right Answeres: 0
+            - Wrong Answeres: 0 (instead of 5)*/
+            wrongAnswers--; 
+        }
+    }
+    /*for loop to check if any of the radio buttons that has been selected is a correct answer*/
+    for (var j = 1 ; j <= numOfQuestions ; j++){
+        if (eval("question" + j) == answers [j - 1]){
+            correctAnswers++;
+        } else {
+            wrongAnswers++;
+        }
+    }
+    //Display the results
+    var results = document.getElementById("results");
+    results.innerHTML = ("<h2> Correct Answers: " + correctAnswers + "<br>" + "Wrong Answers: " + wrongAnswers + "<br>" + "Not Answered: " + unAnswered + "</h2>");
+    //Stop the timer and hide it
+    stop();
+    $("#timer").hide();
+    //Show the restart button
+    $("#restartBtn").show();
+    $("#restartBtn").on("click", function() {
+        location.reload(true);
+    });
+        
+    return false;
+}
+
 //Timer functions
 function runTimer() {
     clearInterval(intervalId);
